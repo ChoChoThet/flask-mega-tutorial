5175f7dfe1fd7ccae35e503b29d05d10899413cd
ChoChoThet <moetharkaungkincho@gmail.com>
2018-06-13 22:38:08 +0630

    Flask Chapter1

--- hello.py
+++ hello.py
@@ -5,5 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello World"
+@app.route("/index")
+def index():
+    return "This is index"
 if __name__ == "__main__":
     app.run()
